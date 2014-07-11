e9fc848a169220a02b64e0f2129df07eaceec935
Caleb Smith <caleb.smithnc@gmail.com>
2014-07-11 14:13:35 -0400

    Adds a first pass at a proper setup.py file

--- setup.py
+++ setup.py
@@ -13,19 +13,19 @@ def read_file(filename):
 
 
 setup(
-    name='django-template_graph',
+    name='django-template-graph',
     version=__import__('template_graph').__version__,
-    author='<Include Your Name Here>',
-    author_email='<Include Your Email Here>',
-    packages=find_packages(),
+    author='Caktus Consulting Group',
+    author_email='solutions@caktusgroup.com',
+    packages=find_packages(exclude=['example']),
     include_package_data=True,
-    url='<Include Link to Project>',
-    license='<Include License Name>',
+    url='https://github.com/caktus/django-template-graph',
+    license='BSD',
     description=u' '.join(__import__('template_graph').__doc__.splitlines()).strip(),
     classifiers=[
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Intended Audience :: Developers',
-        'Programming Language :: Python',      
+        'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Framework :: Django',
